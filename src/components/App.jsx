3f430d0d6bc88d6e41fc564745b85746a5e781a6
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2020-12-30 18:20:24 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  let time = new Date().toLocaleTimeString().substring(0, 7);
+  const [Time, updateTime] = React.useState(time);
+
+  function refreshTime() {
+    const newtime = new Date().toLocaleTimeString().substring(0, 7);
+    updateTime(newtime);
+    return Time;
+  }
+  setInterval(refreshTime, 1000);
+
+  return (
+    <div>
+      <h1 className="container">{Time}</h1>
+    </div>
+  );
+}
+
+export default App;
